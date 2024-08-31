70f6a853412efe9b71164237b858b46df6c0f1f9
J0YD33P <joyslearninghub@gmail.com>
2024-09-01 00:02:40 +0530

    Updated the README

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # etch-a-sketch-top
 Create a sketchpad using HTML, CSS and Javascript
-
-Blueprint
-
-1. Create a 16x16 grid of square divs as the default view. 
-    
-2. If mouse hovers over any of the squares change the color of the square to black
--- script.js
+++ script.js
@@ -12,7 +12,6 @@ for(let i = 0; i<grid; i++){
     for(let j = 0; j<grid; j++){
         const square = document.createElement("div");
         square.classList.add("square");
-    
         square.setAttribute("style", `outline: 1px solid black; width: ${size}px; height: ${size}px;`);
         miniContainer.appendChild(square);
     }
@@ -21,7 +20,7 @@ for(let i = 0; i<grid; i++){
 const elements = document.querySelectorAll(".square");
 
 elements.forEach((element)=> {
-    element.addEventListener("mouseenter", () => 
+    element.addEventListener("mouseout", () => 
         {   element.setAttribute("style", `outline: 1px solid black; width: ${size}px; height: ${size}px; background-color: black;`);
             console.log(element);
         });
